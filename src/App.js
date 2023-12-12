169ab399de574f8a0420f00bb66d521a5b0fb97d
TheAdich <adidadhich2003@gmail.com>
2023-12-12 23:44:33 +0530

    object rearranged

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas , useFrame } from "@react-three/fiber";
 import {
   OrbitControls,
   CameraControls,
   PerspectiveCamera,
   Environment,
 } from "@react-three/drei";
-import { ComputerDesk } from "./ComputerDesk";
-import { AlarmClock } from "./AlarmClock";
-import { Calendar } from "./Calendar";
-import { Computer } from "./Computer";
-import { Cupboard } from "./Cupboard";
-import { Microphone } from "./Microphone";
-import { Phone } from "./Phone";
-import { Trophy } from "./Trophy";
+import { ComputerDesk } from "./models'sJS/ComputerDesk";
+import { AlarmClock } from "./models'sJS/AlarmClock";
+import { Calendar } from "./models'sJS/Calendar";
+import { Computer } from "./models'sJS/Computer";
+import { Cupboard } from "./models'sJS/Cupboard";
+import { Microphone } from "./models'sJS/Microphone";
+import { Phone } from "./models'sJS/Phone";
+import { Trophy } from "./models'sJS/Trophy";
+import React, { useRef } from "react";
+
+
+
+
+
+
+
 export default function App() {
   return (
-    <Canvas flat>
+    <Canvas >
       <OrbitControls />
       <CameraControls minPolarAngle={0} maxPolarAngle={Math.PI} />
       <ambientLight intensity={Math.PI / 2} />
       {/* <directionalLight intensity={2} position={[0, 0, 100]} /> */}
-      {/* <Environment preset="park" background blur={0} /> */}
-      <PerspectiveCamera makeDefault position={[0, 0, -20]} />
-      <ComputerDesk position={[10, 0, 0]} />
-      <AlarmClock position={[10, 2, 0]} />
-      <Calendar />
-      {/* { <Computer></Computer> */}
-      <Cupboard position={[0, 0, -10]}></Cupboard>
-      <Microphone></Microphone>
-      <Phone position={[-10, 0, -10]}></Phone>
-      <Trophy position={[0, 0, -10]}></Trophy>
+       <Environment preset="night" background blur={1} /> 
+      <PerspectiveCamera makeDefault position={[0, 1, -6]} near={1} far={10} />
+      <ComputerDesk position={[0, 0, 0]} />
+      <AlarmClock  position={[-0.75, 0.45, 0]} />
+      <Calendar scale={0.02} position={[0,1,0.5]}/>
+      <Computer scale={0.001} position={[0,0.4,0]} rotation={[0, Math.PI, 0]}/>
+      <Cupboard position={[1.5, 0, -0.7]} rotation={[0, -Math.PI / 1.2 , 0]} scale={0.7}/>
+      <Microphone scale={0.02} position={[0.5,0.4,0]}/>
+      <Phone scale={0.2} position={[-1, 0.5, 0]}></Phone>
+      <Trophy position={[-0.6, 0.4, 0.2]}></Trophy>
     </Canvas>
   );
 }
